b10add1176932fe812e9d5f27efa0e63b91f8a72
Bernhard Miettinen <bernhard.miettinen@gmail.com>
2019-08-06 01:14:40 +0300

    AudioEvent types can now be changed without losing references

--- Editor/AudioEvents/AudioEventEditor.cs
+++ Editor/AudioEvents/AudioEventEditor.cs
@@ -1,4 +1,6 @@
-﻿using UnityEditor;
+﻿using System;
+using System.Linq;
+using UnityEditor;
 using UnityEngine;
 
 namespace Hirame.Apollo.Editor
@@ -7,19 +9,54 @@ namespace Hirame.Apollo.Editor
     public class AudioEventEditor : UnityEditor.Editor
     {
         private static AudioSource PreviewSource;
-
+        private static string[] typeNames;
+        private static Type[] types;
+        
         private AudioEvent audioEvent;
+        private int typeIndex;
+
+        static AudioEventEditor ()
+        {
+            types = (from domainAssembly in AppDomain.CurrentDomain.GetAssemblies()
+                from assemblyType in domainAssembly.GetTypes()
+                where typeof(AudioEvent).IsAssignableFrom(assemblyType) && !assemblyType.IsAbstract
+                select assemblyType).ToArray();
+            
+            typeNames = new string[types.Length];
+            for (var i = 0; i < types.Length; i++)
+            {
+                typeNames[i] = types[i].Name;
+            }
+            
+            Array.Sort (typeNames);
+        }
         
         private void OnEnable ()
         {          
             audioEvent = target as AudioEvent;
+            MatchType ();
+        }
+
+        private void MatchType ()
+        {
+            var typeName = audioEvent.GetType ().Name;
+            
+            for (var i = 0; i < typeNames.Length; i++)
+            {
+                if (!typeNames[i].Equals (typeName))
+                    continue;
+                typeIndex = i;
+            }
         }
 
         public override void OnInspectorGUI ()
-        {    
+        {
             serializedObject.Update ();
 
             DrawPreview ();
+
+            if (ChangeType ())
+                return;
             
             DrawPropertiesExcluding (serializedObject, "m_Script");
             
@@ -35,6 +72,25 @@ namespace Hirame.Apollo.Editor
             {
                 PlayPreview (audioEvent);
             }
+        }
+
+        private bool ChangeType ()
+        {
+            var newTypeIndex = EditorGUILayout.Popup (typeIndex, typeNames);
+
+            if (newTypeIndex == typeIndex)
+                return false;
+
+            typeIndex = newTypeIndex;
+            
+            var multiEvent = CreateInstance (types[typeIndex]);
+            var newId = new SerializedObject (multiEvent).FindProperty ("m_Script").objectReferenceInstanceIDValue;
+            
+            serializedObject.FindProperty ("m_Script").objectReferenceInstanceIDValue = newId;
+            serializedObject.ApplyModifiedProperties ();
+
+            AssetDatabase.Refresh ();
+            return true;
         }
 
         private static void PlayPreview (AudioEvent audioEvent)
